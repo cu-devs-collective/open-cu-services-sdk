21094302a20f6618f91b33d04758afb2772a0d71
zguy <zguy@zguy.dev>
2026-03-09 22:36:37 +0300

    chore(examples): update typescript example

--- examples/typescript/current_student/main.ts
+++ examples/typescript/current_student/main.ts
@@ -3,22 +3,31 @@ import {
   currentStudent
 } from "@cu-devs-collective/open-cu-services-lmsapi";
 
-const bffCookie = process.env.CU_LMS_BFF_COOKIE;
-if (!bffCookie) {
-  throw new Error("CU_LMS_BFF_COOKIE is not set in env");
+async function main() {
+  const bffCookie = process.env.CU_LMS_BFF_COOKIE;
+  if (!bffCookie) {
+    throw new Error("CU_LMS_BFF_COOKIE is not set in env");
+  }
+
+  const client = createLmsApiClient();
+  const result = await currentStudent({
+    client,
+    auth: () => bffCookie
+  });
+
+  if (result.error) {
+    if (result.error instanceof Error) {
+      console.error("currentStudent returned error:");
+      console.error(result.error);
+    } else {
+      console.error(`currentStudent response with status ${result.response?.status}:`);
+      console.error(JSON.stringify(result.error, null, 2));
+    }
+    process.exit(1);
+  }
+
+  console.log(`currentStudent response:`);
+  console.log(JSON.stringify(result.data, null, 2));
 }
 
-const client = createLmsApiClient();
-const result = await currentStudent({
-  client,
-  auth: () => bffCookie
-});
-
-if (result.error) {
-  console.error("currentStudent returned error:");
-  console.error(JSON.stringify(result.error, null, 2));
-  process.exit(1);
-}
-
-console.log(`currentStudent response of type ${result.constructor.name}:`);
-console.log(JSON.stringify(result.data, null, 2));
+await main();
